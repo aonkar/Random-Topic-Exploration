1df066efc791592483a6529b32e9630de2376a6c
Abhishek S Onkar <onkar.s.abhishek@gmail.com>
2019-10-19 14:23:58 +0530

    Update FindPairOfGivenSum.java

--- dsa/src/datastructures/array/FindPairOfGivenSum.java
+++ dsa/src/datastructures/array/FindPairOfGivenSum.java
@@ -12,19 +12,40 @@ public class FindPairOfGivenSum {
 		if (array.length < 2 || array[0] > sum) {
 			return "Not Available";
 		}
-		int i = 0, count = 0;
-		int length = array.length;
-		while (i < length) {
-			for (int j = i + 1; j < length; j++) {
-				if (array[i] + array[j] == sum) {
-					count++;
-				} else if (array[i] + array[j] > sum) {
-					length = j;
+		int left = 0;
+		int right = array.length - 1;
+		int count = 0;
+		int totalIterations = 0;
+		while (left < right) {
+			totalIterations++;
+			if (array[left] + array[right] == sum) {
+				boolean repeat = false;
+				int leftCount = 1;
+				int rightCount = 1;
+				while (array[left] == array[left + 1] && left < right) {
+					repeat = true;
+					leftCount++;
+					left++;
 				}
+				while (array[right] == array[right - 1] && right > left) {
+					repeat = true;
+					rightCount++;
+					right--;
+				}
+				if (repeat) {
+					count = count + leftCount * rightCount;
+				} else {
+					count++;
+				}
+				left++;
+				right--;
+			} else if (array[left] + array[right] > sum) {
+				right--;
+			} else {
+				left++;
 			}
-			i++;
-
 		}
+		System.out.println(totalIterations);
 		return "Number of pairs possible are: " + count;
 	}
 }
