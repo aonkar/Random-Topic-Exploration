9b0dac33d3be2dc2be2090f9098f1e3dd75552db
Abhishek S Onkar <onkar.s.abhishek@gmail.com>
2020-02-07 22:33:39 +0530

    BatCricketer change

--- dsa/src/datastructures/array/matrix/CandidateCode.java
+++ dsa/src/datastructures/array/matrix/CandidateCode.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CandidateCode {
-	public static void main(String[] args) throws Exception {
+	private static int result = 0;
 
+	public static void main(String[] args) throws Exception {
 		Scanner scan = new Scanner(System.in);
 		String firstLine = scan.nextLine();
 		Integer cricketers = Integer.parseInt(firstLine.split(" ")[0]);
@@ -27,6 +28,13 @@ public class CandidateCode {
 					new WeightPrice(Integer.parseInt(batReq.split(" ")[0]), Integer.parseInt(batReq.split(" ")[1])));
 		}
 		scan.close();
+		List<List<WeightPrice>> finalList = getSelectedBats(batsList, cricketersList);
+		findMaxCombination(finalList, finalList.get(0), 0, new ArrayList<WeightPrice>(), 0, batsList.size());
+		System.out.println(result);
+	}
+
+	private static List<List<WeightPrice>> getSelectedBats(List<WeightPrice> batsList,
+			List<WeightPrice> cricketersList) {
 		List<List<WeightPrice>> finalList = new ArrayList<>();
 		for (WeightPrice cricketer : cricketersList) {
 			List<WeightPrice> selectedList = new ArrayList<>();
@@ -39,7 +47,40 @@ public class CandidateCode {
 				finalList.add(selectedList);
 			}
 		}
-		System.out.println("");
+		return finalList;
+	}
+
+	private static void findMaxCombination(List<List<WeightPrice>> finalList, List<WeightPrice> list, int count,
+			List<WeightPrice> listTobeRemoved, int finalCount, int batSize) {
+		list.removeAll(listTobeRemoved);
+		if (count == finalList.size() - 1) {
+			if (list.isEmpty()) {
+				if (result < finalCount) {
+					result = finalCount;
+				}
+			} else {
+				if (result < finalCount + 1) {
+					result = finalCount + 1;
+				}
+			}
+			if (result == batSize) {
+				System.out.println(result);
+				System.exit(0);
+			}
+			return;
+		}
+		if (!list.isEmpty()) {
+			for (int i = 0; i < list.size(); i++) {
+				listTobeRemoved.add(list.get(i));
+				findMaxCombination(finalList, finalList.get(count + 1), count + 1, listTobeRemoved, finalCount + 1,
+						batSize);
+					listTobeRemoved.remove(list.get(i));
+			}
+		} else {
+			findMaxCombination(finalList, finalList.get(count + 1), count + 1, listTobeRemoved, finalCount, batSize);
+
+		}
+
 	}
 
 	public static class WeightPrice {
